72cdb76fdb78251595a60eddb2f597389a953d41
Markus Nordvik <markus@myvr.com>
2013-07-29 16:56:47 -0700

    Adding new file

---
+++ multi.video.js
@@ -0,0 +1,18 @@
+/* var carousel = document.getElementById("property-carousel");
+var carouselParent = carousel.parentNode;
+
+var video = document.createElement("div");
+
+video.innerHTML = '<iframe id="ifrm" name="ifrm" src="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" scrolling="auto"
+ width="790"  height="1024" frameborder="3"><a href="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" target="_blank">Click here for 360 tour</a></iframe>'
+carousel.parentNode.insertBefore(video, carousel.nextSibling); */
+
+var carousel = document.getElementById("property-carousel");
+
+var video = document.createElement("div");
+
+video.innerHTML = '<iframe id="ifrm" name="ifrm" src="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" scrolling="auto"
+ width="790"  height="1024" frameborder="3"><a href="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" target="_blank">Click here for 360 tour</a></iframe>';
+
+carousel.parentNode.insertBefore(video, carousel.nextSibling);
+
