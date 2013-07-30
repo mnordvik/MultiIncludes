c123dc84e9f42e60e712280261483bd8f8662858
Markus <mnordvik@gmail.com>
2013-07-29 17:16:53 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -1,10 +1,9 @@
-
 var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-video.innerHTML = '<iframe id="ifrm" name="ifrm" src="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" scrolling="auto"
- width="790"  height="1024" frameborder="3"><a href="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" target="_blank">Click here for 360 tour</a></iframe>';
+video.innerHTML = '<iframe src="www.google.com" scrolling="auto"
+ width="790"  height="1024" frameborder="3"></iframe>';
 
 carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
