89ec37949c5cdb7e0ead389c49a12824adb232ed
Markus <mnordvik@gmail.com>
2013-07-29 17:30:47 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -4,7 +4,8 @@ var carousel = document.getElementById("recommendationsInfoWindow");
 var video = document.createElement("div");
 
 var iframe = document.createElement("iframe");
-iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");
+/*iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");*/
+iframe.setAttribute("src","http://www.google.com/");
 iframe.setAttribute("scrolling","no");
 
 video.appendChild("iframe");
