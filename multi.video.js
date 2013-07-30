edddf002c9b12d64998f8c64e00ebbc48bed86d6
Markus <mnordvik@gmail.com>
2013-07-29 17:17:33 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -2,8 +2,7 @@ var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-video.innerHTML = '<iframe src="www.google.com" scrolling="auto"
- width="790"  height="1024" frameborder="3"></iframe>';
+video.innerHTML = 'IFRAME NO GOOD?';
 
 carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
