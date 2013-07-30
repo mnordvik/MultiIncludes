8328072339251c487c38268b1bb34b29b04e2199
Markus <mnordvik@gmail.com>
2013-07-29 17:06:39 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -4,15 +4,18 @@ var carouselParent = carousel.parentNode;
 var video = document.createElement("div");
 
 video.innerHTML = '<iframe id="ifrm" name="ifrm" src="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" scrolling="auto"
- width="790"  height="1024" frameborder="3"><a href="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" target="_blank">Click here for 360 tour</a></iframe>'
+ width="790"  height="1024" frameborder="3"><a href="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" target="_blank">Click here for 360 tour</a></iframe>'
+
 carousel.parentNode.insertBefore(video, carousel.nextSibling); */
 
 var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-video.innerHTML = '<iframe id="ifrm" name="ifrm" src="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" scrolling="auto"
+/*video.innerHTML = '<iframe id="ifrm" name="ifrm" src="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" scrolling="auto"
  width="790"  height="1024" frameborder="3"><a href="http://www.visualtour.com/showvt.asp?t=1754762&prt=10003&sk=202" target="_blank">Click here for 360 tour</a></iframe>';
+*/
+video.innerHTML = 'TEST!';
 
 carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
