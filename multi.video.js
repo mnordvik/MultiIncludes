ab4c37e6f2d3165d6a501c725a82719d822886ac
Markus <mnordvik@gmail.com>
2013-07-29 18:23:37 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -1,21 +1,24 @@
 var carousel = document.getElementById("property-carousel");
 
-/*var video = document.createElement("div");*/
-var video = document.createElement("section");
-video.setAttribute("class","toggle-block");
-video.setAttribute("ng-class","{active: !collapse.video}");
+var video = document.createElement("div");
+//var video = document.createElement("section");
+//video.setAttribute("class","toggle-block");
+//video.setAttribute("ng-class","{active: !collapse.video}");
 
 //video.innerHTML = '<section class="toggle-block" ng-class="{active: !collapse.video}">' +
 //            '<a class="opener" ng-click="collapse.video = !collapse.video">' +
-video.innerHTML = '<a class="opener" ng-click="collapse.video = !collapse.video">' +
+/*video.innerHTML = '<a class="opener" ng-click="collapse.video = !collapse.video">' +
             '<h3>Video Tour</h3>' +
             '</a>' +
             '<div class="slide" collapse="collapse.overview">' +
               '<div class="container">' +
  '<iframe src="http://www.visualtour.com/showvt.asp?t=2275330&prt=10003&sk=202" scrolling="auto"' +
- 'width="720" height="800" frameborder="1"></iframe>' +
-'</div></div>';
+ 'width="720" height="600" frameborder="1"></iframe>' +
+'</div></div>';*/
 //'</div></div></section>';
 
-carousel.parentNode.parentNode.insertBefore(video, carousel.parentNode.nextSibling);
+video.innerHTML = '<iframe src="http://www.visualtour.com/showvt.asp?t=2275330&prt=10003&sk=202" scrolling="auto"' +
+ 'width="720" height="550" frameborder="1"></iframe>';
+
+carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
