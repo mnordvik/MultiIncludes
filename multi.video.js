b58eb4b72aa2338afd12b5bc9bbaae58f3991f88
Markus <mnordvik@gmail.com>
2013-07-29 17:43:27 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -2,12 +2,11 @@ var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-var iframe = document.createElement("img");
-/*iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");*/
-iframe.setAttribute("src","http://www.w3schools.com/images/w3schoolslogoNEW310113.gif");
-/*iframe.setAttribute("scrolling","no");*/
-
-video.appendChild("iframe");
+video.innerHTML = '<img src="http://www.w3schools.com/images/w3schoolslogoNEW310113.gif"/>';
+/*var iframe = document.createElement("img");
+iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");
+iframe.setAttribute("scrolling","no");
+video.appendChild("iframe");*/
 
 carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
