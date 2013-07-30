9353b8828f6074b10d76888b63e8f1bb0a64b7ec
Markus <mnordvik@gmail.com>
2013-07-29 17:24:03 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -2,7 +2,7 @@ var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-video.innerHTML = 'IFRAME NO GOOD?';
+video.innerHTML = 'IFRAME NO GOOD';
 
 carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
