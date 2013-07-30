c73f2bc2d7cfbbd9bba50dd14a67306fee1c88f3
Markus <mnordvik@gmail.com>
2013-07-29 18:07:44 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -2,8 +2,15 @@ var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-video.innerHTML = '<iframe src="http://www.visualtour.com/showvt.asp?t=2275330&prt=10003&sk=202" scrolling="auto"' +
- 'width="720" height="800" frameborder="1"></iframe>';
+video.innerHTML = '<section class="toggle-block" ng-class="{active: !collapse.video}">' +
+            '<a class="opener" ng-click="collapse.video = !collapse.video">' +
+              '<h3>Video Tour</h3>' +
+            '</a>' +
+            '<div class="slide" collapse="collapse.overview">' +
+              '<div class="container">' +
++ '<iframe src="http://www.visualtour.com/showvt.asp?t=2275330&prt=10003&sk=202" scrolling="auto"' +
+ 'width="720" height="800" frameborder="1"></iframe>' +
+ '</div></div></section>';
 /*var iframe = document.createElement("img");
 iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");
 iframe.setAttribute("scrolling","no");
