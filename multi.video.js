3555efbf55f38b0887f4e9b719ff6b63887fedc0
Markus <mnordvik@gmail.com>
2013-07-29 18:17:37 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -1,20 +1,21 @@
 var carousel = document.getElementById("property-carousel");
 
-var video = document.createElement("div");
+/*var video = document.createElement("div");*/
+var video = document.createElement("section");
+video.setAttribute("class","toggle-block");
+video.setAttribute("ng-class","{active: !collapse.video}");
 
-video.innerHTML = '<section class="toggle-block" ng-class="{active: !collapse.video}">' +
-            '<a class="opener" ng-click="collapse.video = !collapse.video">' +
-              '<h3>Video Tour</h3>' +
+//video.innerHTML = '<section class="toggle-block" ng-class="{active: !collapse.video}">' +
+//            '<a class="opener" ng-click="collapse.video = !collapse.video">' +
+video.innerHTML = '<a class="opener" ng-click="collapse.video = !collapse.video">' +
+            '<h3>Video Tour</h3>' +
             '</a>' +
             '<div class="slide" collapse="collapse.overview">' +
               '<div class="container">' +
  '<iframe src="http://www.visualtour.com/showvt.asp?t=2275330&prt=10003&sk=202" scrolling="auto"' +
  'width="720" height="800" frameborder="1"></iframe>' +
- '</div></div></section>';
-/*var iframe = document.createElement("img");
-iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");
-iframe.setAttribute("scrolling","no");
-video.appendChild("iframe");*/
+'</div></div>';
+//'</div></div></section>';
 
 carousel.parentNode.parentNode.insertBefore(video, carousel.parentNode.nextSibling);
 
