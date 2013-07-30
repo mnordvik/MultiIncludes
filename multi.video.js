e1c641ff04826b352c4264314fd15f2ae30e1d5e
Markus <mnordvik@gmail.com>
2013-07-29 17:40:23 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -3,10 +3,10 @@ var carousel = document.getElementById("recommendationsInfoWindow");
 
 var video = document.createElement("div");
 
-var iframe = document.createElement("iframe");
+var iframe = document.createElement("img");
 /*iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");*/
-iframe.setAttribute("src","http://www.google.com/");
-iframe.setAttribute("scrolling","no");
+iframe.setAttribute("src","http://www.w3schools.com/images/w3schoolslogoNEW310113.gif");
+/*iframe.setAttribute("scrolling","no");*/
 
 video.appendChild("iframe");
 
