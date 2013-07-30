3194dc28e73078e6475caf5b0043fd906faeacab
Markus <mnordvik@gmail.com>
2013-07-29 17:27:10 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -2,7 +2,11 @@ var carousel = document.getElementById("property-carousel");
 
 var video = document.createElement("div");
 
-video.innerHTML = 'IFRAME NO GOOD';
+var iframe = document.createElement("iframe");
+iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");
+iframe.setAttribute("scrolling","no");
+
+video.appendChild("iframe");
 
 carousel.parentNode.insertBefore(video, carousel.nextSibling);
 
