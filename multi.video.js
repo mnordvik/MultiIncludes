ee313ab9b5ddc9fc65d9717be0111e9ed134d710
Markus <mnordvik@gmail.com>
2013-07-29 17:57:21 -0700

    Update multi.video.js

--- multi.video.js
+++ multi.video.js
@@ -3,7 +3,7 @@ var carousel = document.getElementById("property-carousel");
 var video = document.createElement("div");
 
 video.innerHTML = '<iframe src="http://www.visualtour.com/showvt.asp?t=2275330&prt=10003&sk=202" scrolling="auto"' +
- 'width="790" height="1024" frameborder="1"></iframe>';
+ 'width="720" height="800" frameborder="1"></iframe>';
 /*var iframe = document.createElement("img");
 iframe.setAttribute("src","http://www.visualtour.com/applets/flashviewer2/viewer.asp?t=1754762&sk=202");
 iframe.setAttribute("scrolling","no");
