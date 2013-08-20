3d1f0dd39b8ae4e9b9070019da1e1a744556f16b
Markus <mnordvik@gmail.com>
2013-08-20 01:42:53 -0700

    Update set.bg.js

--- sleepingladyvilla/set.bg.js
+++ sleepingladyvilla/set.bg.js
@@ -1,5 +1,5 @@
-document.getElementById('backstretch').firstElementChild.setAttribute("src", "https://s3.amazonaws.com/jomumist-myvr-prod-photos/a052ea48656d2744/13157b13e13aee9d/9186A6B4E1.jpg");
-alert(document.getElementById('backstretch').innerHTML);
+//document.getElementById('backstretch').firstElementChild.setAttribute("src", "https://s3.amazonaws.com/jomumist-myvr-prod-photos/a052ea48656d2744/13157b13e13aee9d/9186A6B4E1.jpg");
+//alert(document.getElementById('backstretch').innerHTML);
 
 // <![CDATA[
     $(document).ready(function() {
