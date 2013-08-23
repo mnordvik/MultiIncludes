1f54bfa0e6fcfdbafdb557b6987a564af2cc622a
Markus <mnordvik@gmail.com>
2013-08-23 14:26:15 -0700

    Update styles.js

--- floatingstonecasitas/styles.js
+++ floatingstonecasitas/styles.js
@@ -2,25 +2,27 @@ document.getElementById("header").style.display='none';
 document.getElementById("search").style.display='none';
 document.getElementById("footer").style.display='none';
 
-var content = document.getElementById("content");
+var pathArray = window.location.href.split('/');
+var finalDirectory;
 
-if(typeof content !== "undefined" && content != null) {
+if(pathArray[pathArray.length - 1] != '')
+    finalDirectory = pathArray[pathArray.length - 1];
+else
+    finalDirectory = pathArray[pathArray.length - 2];
 
-  var mainTitle = content.firstChild.nextSibling;
+if(finalDirectory != 'property') {
+
+  var content = document.getElementById("content");
   
-  var backLink = document.createElement("div");
-  backLink.style.fontSize='22px';
-  backLink.style.paddingLeft='150px';
-  backLink.innerHTML = '<a href="http://floatingstonecasitas.myvr.com/property/" style="font-size:22px;padding-left:150px">Back to all properties</a>';
-
-  //alert(mainTitle.innerHTML);
-  mainTitle.appendChild(backLink);
-  /*var holder = mainTitle.firstChild;
-
-  if (holder.nextSibling) {
-    holder.parentNode.insertBefore(backLink, holder.nextSibling);
-  } else {
-    holder.parentNode.appendChild(backLink);
-  }*/
+  if(typeof content !== "undefined" && content != null) {
+  
+    var mainTitle = content.firstChild.nextSibling;
+    
+    var backLink = document.createElement("div");
+    backLink.innerHTML = '<a href="http://floatingstonecasitas.myvr.com/property/" style="font-size:22px;padding-left:150px">Back to all properties</a>';
+  
+    mainTitle.appendChild(backLink);
+  
+  }
 
 }
