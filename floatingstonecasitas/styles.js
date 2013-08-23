e397f2a66c7d27035a796570ac5ceb0df22571f5
Markus <mnordvik@gmail.com>
2013-08-23 12:23:00 -0700

    Update styles.js

--- floatingstonecasitas/styles.js
+++ floatingstonecasitas/styles.js
@@ -1,3 +1,20 @@
 document.getElementById("header").style.display='none';
 document.getElementById("search").style.display='none';
 document.getElementById("footer").style.display='none';
+
+var mainTitle = document.getElementById("main-title");
+
+if(typeof mainTitle !== "undefined") {
+
+  var backLink = document.createElement("div");
+  backLink.innerHTML = '<a href="http://floatingstonecasitas.myvr.com/property/">Back to all properties</a>';
+
+  var holder = mainTitle.firstChild;
+
+  if (holder.nextSibling) {
+    holder.parentNode.insertBefore(backLink, holder.nextSibling);
+  } else {
+    holder.parentNode.appendChild(backLink);
+  }
+
+}
