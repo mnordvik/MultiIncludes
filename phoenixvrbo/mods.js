9171c28d603258d3c646ad20b12051bd6228f60d
Markus <mnordvik@gmail.com>
2013-09-23 15:52:24 -0700

    Update mods.js

--- phoenixvrbo/mods.js
+++ phoenixvrbo/mods.js
@@ -1,4 +1,4 @@
 var tab2 = document.getElementById("tab2");
-var recs = tab2.getElementsByName("recommendations");
+var recs = tab2.getElementsByTagName("input");
 recs[1].checked = true;
 recs[0].style.display='none';
