057289e66f4f627a025eaad9eb52ec934e89ee34
Markus <mnordvik@gmail.com>
2013-09-24 12:19:35 -0700

    Update mods.js

--- phoenixvrbo/mods.js
+++ phoenixvrbo/mods.js
@@ -1,19 +1,20 @@
 var tab2 = document.getElementById("tab2");
 var theKids = tab2.childNodes;
 
-for (i=0;i<7;i++)
-  theKids[i].parentNode.removeChild(theKids[i]);
+//for (i=0;i<7;i++)
+//  theKids[i].parentNode.removeChild(theKids[i]);
 
-/*for (i=0;i<theKids.length;i++)
+for (i=0;i<theKids.length;i++)
 {
-  if(i!=7) {
+  alert("Nodename:" + theKids[i].nodeName + " value:" + theKids[i].nodeValue + " type:" + theKids[i].nodeType);
+  /*if(i!=7) {
     if(theKids[i].nodeType="Text")
       theKids[i].nodeValue='';
     else
       theKids[i].style.display='none';
-  }
+  }*/
 }
-  theKids[3].checked='true';
-  theKids[7].style.display='inline';
-*/
+//  theKids[3].checked='true';
+  //theKids[7].style.display='inline';
+
 //theKids[3].checked='true';
