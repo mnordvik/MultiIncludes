04e969a0a1c77cbc696da7f13cf4bcb145462324
Markus <mnordvik@gmail.com>
2013-10-11 14:32:27 -0700

    Update style.js

--- yourvacationhome/style.js
+++ yourvacationhome/style.js
@@ -6,19 +6,3 @@ alert(form[1].childNodes[3].childNodes[1].innerHTML);
 
 alert("The 4th Div = "+form[1].childNodes[3].className);
 
-/*alert("# of InquiryForms = " + form.length);
-
-var formSections = form[1].getElementsByTagName("DIV");
-
-alert("# of formSections = " + formSections.length);
-
-var specialRequests = formSections[3];
-
-alert("specialRequests.className = " + specialRequests.className);
-
-var requests = specialRequests.getElementsByTagName("DIV");
-
-alert("requests.length = " + requests.length);
-
-requests[0].innerHTML = 'THIS IS WHERE YOU ARE NOW :(';*/
-
