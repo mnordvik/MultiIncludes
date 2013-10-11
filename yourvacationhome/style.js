1037222b0d41b8ee12ad16133d2b1956ef56e30f
Markus <mnordvik@gmail.com>
2013-10-11 14:37:13 -0700

    Update style.js

--- yourvacationhome/style.js
+++ yourvacationhome/style.js
@@ -1,5 +1,7 @@
 var form = document.getElementsByName("InquiryForm");
 
+alert(form[1].childNodes[3].innerHTML);
+
 alert(form[1].childNodes[3].childNodes[1].innerHTML);
 
 alert("The 4th Div = "+form[1].childNodes[3].className);
