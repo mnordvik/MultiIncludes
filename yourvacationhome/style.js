2817711aca5e49943969f1cc0ae06f9fbb55bd34
Markus <mnordvik@gmail.com>
2013-10-11 14:00:32 -0700

    Update style.js

--- yourvacationhome/style.js
+++ yourvacationhome/style.js
@@ -2,4 +2,4 @@ var form = document.getElementsByName("InquiryForm");
 
 var specialRequests = form[0].getElementsByTagName("div")[3];
 
-specialRequests..getElementsByTagName("div")[1].style.visibility='hidden';
+specialRequests..getElementsByTagName("div")[1].innerHTML = '';
