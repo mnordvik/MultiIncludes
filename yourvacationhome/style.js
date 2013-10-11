c12db42a0a032a0f3fa5a63048861fd46ccaca21
Markus <mnordvik@gmail.com>
2013-10-11 14:57:13 -0700

    Update style.js

--- yourvacationhome/style.js
+++ yourvacationhome/style.js
@@ -1,11 +1,13 @@
 var form = document.getElementsByName("InquiryForm");
 
-alert(form[1].childNodes[7].innerHTML);
+//alert(form[1].childNodes[7].innerHTML);
 
-alert(form[1].childNodes[7].childNodes[3].innerHTML);
+//alert(form[1].childNodes[7].childNodes[3].innerHTML);
+
+form[1].childNodes[7].childNodes[3].innerHTML = '';
 
 var moreInfo = document.getElementById("more_info_tab");
-moreInfo.innerHTML = '<span>HOME</span><br/>"VIDEO"';
+moreInfo.innerHTML = 'HOME<br/><span>VIDEO</span>';
 
 //alert("The 4th Div = "+form[1].childNodes[3].className);
 
