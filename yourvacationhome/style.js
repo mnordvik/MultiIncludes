c339604b95d08690f4a701261af1c4abab2dc0e6
Markus Nordvik <markus@myvr.com>
2013-10-11 13:43:31 -0700

    more js

---
+++ yourvacationhome/style.js
@@ -0,0 +1,5 @@
+var form = document.getElementsByName("InquiryForm");
+
+var specialRequests = form[0].getElementsByTagName("div")[3];
+
+specialRequests..getElementsByTagName("div")[1].style.visibility='hidden';
