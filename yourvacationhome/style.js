f44b92a96a39dace8a7bd0669cdcf2a173f03c1b
Markus <mnordvik@gmail.com>
2013-10-17 00:46:21 -0700

    Update style.js

--- yourvacationhome/style.js
+++ yourvacationhome/style.js
@@ -1,13 +1,14 @@
+// Remove smoking/pets from inquiry form
 var form = document.getElementsByName("InquiryForm");
-
-//alert(form[1].childNodes[7].innerHTML);
-
-//alert(form[1].childNodes[7].childNodes[3].innerHTML);
-
 form[1].childNodes[7].childNodes[3].innerHTML = '';
 
+// Change More Info to Home Video in the topnav
 var moreInfo = document.getElementById("more_info_tab");
 moreInfo.innerHTML = 'HOME<br/><span>VIDEO</span>';
+
+// Add Superior Vacation Home logo in footer
+var footerThirdSection = document.getElementById("footer_link_section_three");
+footerThirdSection.innerHTML = footerThirdSection.innerHTML + '<div><a href="http://superiorvacationhome.com/"><img src="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/superior_vacation_home.png"/></a></div>';
 
 //alert("The 4th Div = "+form[1].childNodes[3].className);
 
