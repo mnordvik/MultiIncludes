3abc2a4af4aa43fcaff15b2a3c58f0228ee26f11
Markus <mnordvik@gmail.com>
2013-11-21 18:02:18 -0800

    Update style.js

--- champion-villas/style.js
+++ champion-villas/style.js
@@ -1,3 +1,4 @@
 // Remove smoking/pets from inquiry form
 $("div label:contains('Pets')").remove();
 $("div label:contains('Smokers')").remove();
+$("div label:contains('Special requests')").remove();
