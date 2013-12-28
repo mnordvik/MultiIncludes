37c81bac98e88fe350550e154e624dd445dd0f7e
Markus <mnordvik@gmail.com>
2013-12-27 18:31:57 -0800

    Update indiv_style.js

--- premiumbeachcondos/indiv_style.js
+++ premiumbeachcondos/indiv_style.js
@@ -1,1 +1,24 @@
-$( "#more_info_tab" ).html( "<span>VIRTUAL</span><br/>TOUR" );
+TourLinks = function() {
+  /* Private data. */
+  var data = {
+    "calypso508east": "http://tours.ecrep.co/public/vtour/display/163131?a=1",
+    "calypso605east": "http://tours.ecrep.co/public/vtour/display/163139?a=1",
+    "calypso1007e": "http://tours.ecrep.co/public/vtour/display/163137?a=1"
+  }
+  /* Get a link for a tour by property slug, or notify that there is none */
+  this.getLink = function(slug) {
+    if(data.hasOwnProperty(slug)) { return data[slug]; }
+    return "none";
+  }
+}
+
+var slug = window.location.split('.')[0].split('//')[1];
+var tourLinks = new TourLinks();
+var link = tourLinks.getLink(slug);
+
+if(link != "none")
+  $( "#selected_photo_container" )
+    .append( "<div style=\"text-align:center\"><a href=\""
+    + tourLinks.getLink
+    );
+
