f96a5f7dc90f47bec8de4edfa74d1ccaac40587a
Markus <mnordvik@gmail.com>
2014-01-15 14:42:18 -0800

    Update style.js

--- smokymountaintnvacationrentals/style.js
+++ smokymountaintnvacationrentals/style.js
@@ -1,5 +1,7 @@
 $("div#text-holder").each(function() {
+    
     var $this = $(this);
+    console.log($this.html());
     $this.html($this.html().replace(/&nbsp;/g, ' '));
     $this.html($this.html().replace(/&amp;/g, '&'));
 });
