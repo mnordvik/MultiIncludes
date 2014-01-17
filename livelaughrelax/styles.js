8614c75fcec849fcad00181e132cad4fed9efce4
Markus <mnordvik@gmail.com>
2014-01-17 00:10:31 -0800

    Update styles.js

--- livelaughrelax/styles.js
+++ livelaughrelax/styles.js
@@ -1,1 +1,1 @@
-$('#home_info_image').css('background-image', 'url(' + https://s3.amazonaws.com/jomumist-myvr-prod-photos/048851a2c0d5aaaf/ee01dce859114a82/static/main_room.jpg + ')');
+$('#home_info_image').css('background-image', 'url(' + 'https://s3.amazonaws.com/jomumist-myvr-prod-photos/048851a2c0d5aaaf/ee01dce859114a82/static/main_room.jpg' + ')');
