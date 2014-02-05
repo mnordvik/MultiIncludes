41213bd647111ad8d647562a6a3da5e3720a346f
Markus <mnordvik@gmail.com>
2014-02-05 11:47:06 -0800

    Update style.js

--- eatlovestay/style.js
+++ eatlovestay/style.js
@@ -1,1 +1,1 @@
-$(".top-level-nav").append("<div><a href=\"#\"><img src=\"http://socialchamps.com/wp-content/uploads/2013/09/fb1.png\" border=\"0\" /></a></div>");
+$(".top-level-nav").append("<div style=\"float:right;\"><a href=\"#\"><img src=\"http://socialchamps.com/wp-content/uploads/2013/09/fb1.png\" border=\"0\" /></a></div>");
