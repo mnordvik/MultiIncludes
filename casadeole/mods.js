59baf80d80e6148364c6f99bd3b13314f5ce487c
Markus <mnordvik@gmail.com>
2014-02-05 22:58:27 -0800

    Update mods.js

--- casadeole/mods.js
+++ casadeole/mods.js
@@ -1,1 +1,1 @@
-$(".nav > li:contains('/property/')").html("<a href=\"/property/casa-de-ole/\" target=\"_self\">Property</a>);
+$(".nav li:contains('/property/')").html("<a href=\"/property/casa-de-ole/\" target=\"_self\">Property</a>);
