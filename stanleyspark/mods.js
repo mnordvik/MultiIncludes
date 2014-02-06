423ebf6b42dd3a04b4b7c6ab8a0d307ab98c68a4
Markus <mnordvik@gmail.com>
2014-02-05 17:40:19 -0800

    Update mods.js

--- stanleyspark/mods.js
+++ stanleyspark/mods.js
@@ -1,3 +1,3 @@
 $(".jumbotron div").append("<p><strong>Call 844-744-8787</strong></p>");
 $(".jumbotron > .container > h1").remove();
-$(".jumbotron > .container").prepend("<div><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/stanleyspark/TPVR_logo.jpg\" style=\"max-width:125px;max-height:125px;\"/></div>");
+$(".jumbotron > .container").prepend("<div><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/stanleyspark/TPVR_logo.jpg\" style=\"max-width:125px;max-height:125px;margin: 0 0 10px;\"/></div>");
