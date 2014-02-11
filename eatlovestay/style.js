8ebab0da8187f0519210ae371191806588f25c20
Markus <mnordvik@gmail.com>
2014-02-11 10:32:01 -0800

    Update style.js

--- eatlovestay/style.js
+++ eatlovestay/style.js
@@ -2,7 +2,7 @@ $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://www.fa
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://twitter.com/eatlovestayus\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/twitter.png\" border=\"0\" /></a></div>");
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"http://www.linkedin.com/company/eat-love-stay-home-rentals?trk=cp_followed_name_eat-love-stay-home-rentals\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/linkedin.png\" border=\"0\" /></a></div>");
 
-if(document.indexOf("sample_background.png") != 0)
+if(document.documentElement.innerHTML.indexOf("sample_background.png") != 0)
   console.log("found it");
 
-$("#page-banner").css("background-image", "url(\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/hollywood_hills.jpg\")");
+$(".page-banner").css("background-image", "url(\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/hollywood_hills.jpg\")");
