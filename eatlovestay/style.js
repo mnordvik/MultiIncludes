d184bec6cbb6d996e7437b9d0a2b8a43efd80da4
Markus <mnordvik@gmail.com>
2014-02-10 16:13:06 -0800

    Update style.js

--- eatlovestay/style.js
+++ eatlovestay/style.js
@@ -1,1 +1,3 @@
-$(".top-level-nav").append("<div style=\"float:right;\"><a href=\"#\"><img src=\"http://socialchamps.com/wp-content/uploads/2013/09/fb1.png\" border=\"0\" /></a></div>");
+$(".top-level-nav").append("<div style=\"float:right;\"><a href=\"https://www.facebook.com/EatLoveStay\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/facebook.png\" border=\"0\" /></a></div>");
+$(".top-level-nav").append("<div style=\"float:right;\"><a href=\"https://twitter.com/eatlovestayus\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/twitter.png\" border=\"0\" /></a></div>");
+$(".top-level-nav").append("<div style=\"float:right;\"><a href=\"http://www.linkedin.com/company/eat-love-stay-home-rentals?trk=cp_followed_name_eat-love-stay-home-rentals\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/linkedin.png\" border=\"0\" /></a></div>");
