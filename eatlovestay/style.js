4a6957ee42e21a687ec872b4958c7590e7709d1e
Markus <mnordvik@gmail.com>
2014-02-11 10:38:22 -0800

    Update style.js

--- eatlovestay/style.js
+++ eatlovestay/style.js
@@ -3,6 +3,4 @@ $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://twitte
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"http://www.linkedin.com/company/eat-love-stay-home-rentals?trk=cp_followed_name_eat-love-stay-home-rentals\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/linkedin.png\" border=\"0\" /></a></div>");
 
 if(document.documentElement.innerHTML.indexOf("sample_background.png") != 0)
-  console.log("found it");
-
-$(".page-banner").css("background-image", "url(\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/hollywood_hills.jpg\")");
+  $(".page-banner").css("background-image", "url(\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/hollywood_hills.jpg\")");
