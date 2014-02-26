1dde51fa9cc4f18a6b902d6e2cd4d58dab2cda1c
Markus <mnordvik@gmail.com>
2014-02-26 14:14:54 -0800

    Update style.js

--- eatlovestay/style.js
+++ eatlovestay/style.js
@@ -2,6 +2,10 @@ $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://www.fa
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://twitter.com/eatlovestayus\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/twitter.png\" border=\"0\" /></a></div>");
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"http://www.linkedin.com/company/eat-love-stay-home-rentals?trk=cp_followed_name_eat-love-stay-home-rentals\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/linkedin.png\" border=\"0\" /></a></div>");
 
+console.log($(.footer-links li).html());
+console.log(document.documentElement.innerHTML.indexOf('href=\"/blog'));
+//console.log();
+
 $(.footer-links li).html().replace('href=\"/blog', 'href=\"http://blog.eatlovestay.com');
 
 if(document.documentElement.innerHTML.indexOf("sample_background.png") != 0)
