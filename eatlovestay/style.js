b75797353041173050c8d9db76f0383ffff92749
Markus <mnordvik@gmail.com>
2014-02-27 12:06:21 -0800

    Update style.js

--- eatlovestay/style.js
+++ eatlovestay/style.js
@@ -2,16 +2,13 @@ $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://www.fa
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"https://twitter.com/eatlovestayus\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/twitter.png\" border=\"0\" /></a></div>");
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"http://www.linkedin.com/company/eat-love-stay-home-rentals?trk=cp_followed_name_eat-love-stay-home-rentals\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/linkedin.png\" border=\"0\" /></a></div>");
 
-//console.log(document.documentElement.innerHTML.indexOf('href=\"/blog'));
-//console.log();
-
 $(".footer-links li").each(function() {
   var anchor = $(this).html();
   console.log("OLD " + anchor);
   console.log(anchor.indexOf('href=\"/blog'));
-  anchor.replace('href=\"/blog', 'href=\"http://blog.eatlovestay.com');
-  console.log("NEW " + anchor);
-  $(this).html(anchor);
+  var newAnchor = anchor.replace('href=\"/blog', 'href=\"http://blog.eatlovestay.com');
+  console.log("NEW " + newAnchor);
+  $(this).html(newAnchor);
 });
 
 if(document.documentElement.innerHTML.indexOf("sample_background.png") > -1)
