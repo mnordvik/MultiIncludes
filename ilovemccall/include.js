f68866d8eb34d983a7e994f58fbfd35549808a1d
Markus <mnordvik@gmail.com>
2014-03-10 15:28:50 -0700

    Update include.js

--- ilovemccall/include.js
+++ ilovemccall/include.js
@@ -1,1 +1,2 @@
-<script src="https://lets.paybygroup.com/snippet/v1/loader.js" data-merchant-id="frostmanagement"></script>
+var scripttext = "<script src=\"https://lets.paybygroup.com/snippet/v1/loader.js\" data-merchant-id=\"frostmanagement\"></script>";
+$("head").append($("<script>"+scripttext+"</script>"));
