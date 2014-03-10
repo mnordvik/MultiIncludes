ccd538427f0d6c858270e9b4406440bad647e4c6
Markus <mnordvik@gmail.com>
2014-03-10 15:54:16 -0700

    Update include.js

--- ilovemccall/include.js
+++ ilovemccall/include.js
@@ -1,4 +1,8 @@
-<script type='text/javascript' src='https://lets.paybygroup.com/snippet/v1/loader.js' data-merchant-id='frostmanagement'></script>
-//var scriptText = "<script src=\"https://lets.paybygroup.com/snippet/v1/loader.js\" data-merchant-id=\"frostmanagement\"></script>";
-//var newText = "<script>" + scriptText + "</script>";
-//$("head").append(newText);
+var scriptText = "<script src=\"https://lets.paybygroup.com/snippet/v1/loader.js\" data-merchant-id=\"frostmanagement\"></script>";
+var newText = "<script>" + scriptText + "</script>";
+
+  var head= document.getElementsByTagName('head')[0];
+   var script= document.createElement('script');
+   script.type= 'text/javascript';
+   script.src= 'helper.js';
+   head.appendChild(newText);
