0335f1883cac1a6629e5d5fe979034534be9977a
Markus <mnordvik@gmail.com>
2014-03-28 13:43:36 -0700

    Update style.js

--- losangelesholidayrental/style.js
+++ losangelesholidayrental/style.js
@@ -1,2 +1,2 @@
 // Add third-party blog link to the topnav
-$("  <a href='http://Blog.LosAngelesHolidayRental.com' target='_blank'><div id='blog_tab' class='top_menu_component'>OUR<br/><span>BLOG</span></div></a> ").insertAfter(" #inquire_tab ");
+$("  <a href='http://Blog.LosAngelesHolidayRental.com' target='_blank'><div class='top_menu_component'>OUR<br/><span>BLOG</span></div></a> ").insertAfter(" #inquire_tab ");
