7fad70b8abffe1710750aa46d6d442919af58467
Markus <mnordvik@gmail.com>
2014-04-02 13:20:33 -0700

    Update mods.js

--- stanleyspark/mods.js
+++ stanleyspark/mods.js
@@ -3,6 +3,6 @@ $(".jumbotron > .container > h1").remove();
 $(".jumbotron > .container").prepend("<div><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/stanleyspark/TPVR_logo.jpg\" style=\"max-width:125px;max-height:125px;margin: 0 0 10px;\"/></div>");
 */
 
-$(".nav-bar .nav-bar-defualt .container").append("<div class=\"social-icons\"><a href=\"\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/facebook.png\" border=\"0\" /></a></div>");
+$("#site-content-container > .container").append("<div class=\"social-icons\"><a href=\"\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/facebook.png\" border=\"0\" /></a></div>");
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/twitter.png\" border=\"0\" /></a></div>");
 $(".top-level-nav").append("<div class=\"social-icons\"><a href=\"\"><img src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/eatlovestay/linkedin.png\" border=\"0\" /></a></div>");
