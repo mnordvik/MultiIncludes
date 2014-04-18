ce8359ddfb3c40c9f4c8d5916a2a968b65266e28
Markus <mnordvik@gmail.com>
2014-04-18 01:27:47 -0700

    Create style.js

---
+++ cardinalsviewcottage/style.js
@@ -0,0 +1,2 @@
+$(".author").before("<div class=\"social-icons\"><a href=\"https://www.facebook.com/pages/Cardinals-View-Cottage/180160668680200\"><i class='icon-facebook-sign'></i></a></div>");
+$(".author").before("<div class=\"social-icons\"><a href=\"https://plus.google.com/b/118189753820727586827/118189753820727586827/about/p/pub\"><i class='icon-google-plus'></i></a></div>");
