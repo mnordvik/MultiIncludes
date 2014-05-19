19d8a69ce28ecc31d0197cafe974552c42bfeb7b
Markus <mnordvik@gmail.com>
2014-05-19 10:08:44 -0700

    Update style.js

--- avalonca/style.js
+++ avalonca/style.js
@@ -1,4 +1,4 @@
 $("#sq_f_size").remove();
 $("#sq_f_suff").remove();
 
-$("#more_info_tab").html("<span>ISLAND</span><br/>\"HISTORY\"");
+$("#more_info_tab").html("<span>ISLAND</span><br/>HISTORY");
