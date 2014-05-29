419774b765a8ae966dfce3bcacc0210cfec89c3f
Markus <mnordvik@gmail.com>
2014-05-29 13:07:52 -0700

    Update style.js

--- wallingfordguesthouse/style.js
+++ wallingfordguesthouse/style.js
@@ -1,2 +1,2 @@
 
-$("#top_social > .fr").append("<div class=\"social-icons\"><a href=\"https://www.facebook.com/PremiumBeachCondos\"><img style=\"height:20%;width:20%;\"src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/wallingfordguesthouse/facebook-button.png\"/></a></div>");
+$("#top_social > .fl").append("<div class=\"social-icons\"><a href=\"https://www.facebook.com\"><img style=\"height:35%;width:35%;\"src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/wallingfordguesthouse/facebook-button.png\"/></a></div>");
