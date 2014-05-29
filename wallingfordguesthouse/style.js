6bcb08b2a1b59255fbb3f79bc4048a321b9e5ddf
Markus <mnordvik@gmail.com>
2014-05-29 12:31:32 -0700

    Update style.js

--- wallingfordguesthouse/style.js
+++ wallingfordguesthouse/style.js
@@ -1,1 +1,1 @@
-$(" #top-social .fr ").append("  <a class='top_menu_component' onClick='window.location.href =\"http://Blog.LosAngelesHolidayRental.com\"; return false;'><div>OUR<br/><span>BLOG</span></div></a> ");
+$(" #top_social .fr ").append("  <a class='top_menu_component' onClick='window.location.href =\"http://Blog.LosAngelesHolidayRental.com\"; return false;'><div>OUR<br/><span>BLOG</span></div></a> ");
