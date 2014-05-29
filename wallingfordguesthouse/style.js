04c96ffd3c989fb5349eaa492ab80a3aab262a15
Markus <mnordvik@gmail.com>
2014-05-29 13:15:55 -0700

    Update style.js

--- wallingfordguesthouse/style.js
+++ wallingfordguesthouse/style.js
@@ -1,2 +1,2 @@
 
-$("#top_social > .fl").append("<div class=\"social-icons\"><a href=\"https://www.facebook.com\"><img style=\"height:35%;width:35%;\"src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/wallingfordguesthouse/facebook-button.png\"/></a></div>");
+$("#top_social > .fl").append("<a href=\"https://www.facebook.com\" class=\"social-icons\"><img style=\"height:35%;width:35%;\"src=\"https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/wallingfordguesthouse/facebook-button.png\"/></a>");
