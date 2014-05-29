2ba1394e1981d8a4fcaf9f5cfd8a34e7953eb233
Markus <mnordvik@gmail.com>
2014-05-29 14:13:14 -0700

    Update style.js

--- lagiralda/style.js
+++ lagiralda/style.js
@@ -1,1 +1,1 @@
-$(".navbar-header").append("<div class=\"social-icons\"><a href=\"http://instagram.com/bayislandstar\"><i class='icon-instagram'></i></a></div>");
+$(".top-level-nav").append("<div class=\"social-icons\"><a href=\"http://instagram.com/bayislandstar\"><i class='icon-instagram'></i></a></div>");
