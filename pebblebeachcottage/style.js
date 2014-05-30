3fc6d2cf039cb0a30c5b334b17a8c4612bfc11bf
Markus <mnordvik@gmail.com>
2014-05-29 23:30:36 -0700

    Update style.js

--- pebblebeachcottage/style.js
+++ pebblebeachcottage/style.js
@@ -2,4 +2,5 @@ $(" #calendar_wrapper ").remove();
 $(" #estimate_wrapper ").remove();
 
 var calendarTab = $(" #calendar_tab ").innerHTML.replace("Calendar","Fees");
+console.log(calendarTab);
 $(" #calendar_tab ").innerHTML = calendarTab;
