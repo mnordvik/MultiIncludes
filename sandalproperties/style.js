792f8ca67727f40010eedb8e9fa57a74c28e9df4
Markus <mnordvik@gmail.com>
2014-05-30 10:54:42 -0700

    Update style.js

--- sandalproperties/style.js
+++ sandalproperties/style.js
@@ -1,3 +1,3 @@
 $(" #tab2 > input ").hide();
-$(" #tab2 ").html().replace("Community Provided","");
+$(" #tab2 ").html($(" #tab2 ").html().replace("Community Provided",""));
 $(" #tab2 ").html().replace("Owner Provided","");
