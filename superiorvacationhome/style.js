8bfaab2cd607c7caf17ffa6b6c17981388d40c9b
Markus <mnordvik@gmail.com>
2014-06-18 12:59:58 -0700

    Update style.js

--- superiorvacationhome/style.js
+++ superiorvacationhome/style.js
@@ -1,2 +1,20 @@
 var stockTo = 'List Your Home';
 $('ul li:contains(' + stockTo + ')').remove();
+
+<!-- Start of StatCounter Code for Frontpage 2002 -->
+//<script type="text/javascript">
+var sc_project=9804248;
+var sc_invisible=1;
+var sc_security="1704efb3";
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+//</script>
+/*<noscript><div class="statcounter"><a title="web analytics"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/9804248/0/1704efb3/1/"
+alt="web analytics"></a></div></noscript>*/
+<!-- End of StatCounter Code for Frontpage 2002 -->
