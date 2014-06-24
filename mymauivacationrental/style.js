395825e5bf77cf23f96befefa4d4b5d90eebf877
Markus <mnordvik@gmail.com>
2014-06-24 13:24:23 -0700

    Update style.js

--- mymauivacationrental/style.js
+++ mymauivacationrental/style.js
@@ -1,1 +1,4 @@
-$("#favicon").attr("href","https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/mymauivacationrental/favicon.ico");
+//$("#favicon").attr("href","https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/mymauivacationrental/favicon.ico");
+
+$('#favicon').remove();
+$('<link id="favicon" type="image/x-icon" rel="shortcut icon" href="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/mymauivacationrental/favicon.ico" />').appendTo('head');
