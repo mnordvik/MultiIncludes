027050eaf5640e48e3e39bde727add07301b5e92
Markus <mnordvik@gmail.com>
2014-07-15 11:11:11 -0700

    Update style.js

--- fourwindsniagara/style.js
+++ fourwindsniagara/style.js
@@ -1,1 +1,1 @@
-$('div.carousel-caption').wrapInner('<span style="background-color:rgba(0,0,0,0.1);padding:0px;" />');
+$('div.carousel-caption').wrapInner('<span style="background-color:rgba(0,0,0,0.1);padding:0px 10px 5px 10px;" />');
