d4409090d04d0a23dcf299ddbfe0d9ff9f50e761
Markus <mnordvik@gmail.com>
2014-07-18 16:32:03 -0700

    Update style.js

--- justitalianrentals/style.js
+++ justitalianrentals/style.js
@@ -1,9 +1,9 @@
 console.log("INNNNN");
 var pathElements = location.pathname.split("/");
 console.log(pathElements);
-if (pathElements.length > 1 && pathElements[0] == "property") {
-  console.log(pathElements[0]);
-  console.log(document.domain + "/" + pathElements[1] + "/");
+if (pathElements.length > 1 && $.inArray("property", pathElements) {
+  console.log(pathElements[1]);
+  console.log(document.domain + "/" + pathElements[2] + "/");
   $('.inquiry-container').innerHTML(
     "<h3>Stay With Us!</h3>"
     + "<div class=\"row text-center\"><a class=\"btn btn-primary btn-lg rounded-bigger\" href=\"\">Book Now</a></div>"
