bc039d71d15e7c577701ac1d576f3894181c08b8
Markus <mnordvik@gmail.com>
2014-07-18 17:50:04 -0700

    Update style.js

--- justitalianrentals/style.js
+++ justitalianrentals/style.js
@@ -4,8 +4,8 @@ if (pathElements.length > 1 && pathElements.indexOf('property')!==-1) {
   var bookingPage = document.domain + "/" + pathElements[pathElements.indexOf('property')+1] + "/";
   console.log("bookingPage = " + bookingPage);
   $('.inquiry-container').html(
-    "<div class=\"row text-center\"><a href=\"" + bookingPage + "\" target=\"_self\">"
-    + "<div>Book Now</div></a></div>"
+    "<div class=\"row text-center\"><br/><a href=\"" + bookingPage + "\" target=\"_self\">"
+    + "<div class=\"btn btn-primary btn-lg rounded-bigger\">Book Now</div></a></div>"
     + "<div class=\"row text-center\"><br/>or call us</div>"
     + "<div class=\"row text-center\"><h3>0345 869 1463</h3></div>"
   );
