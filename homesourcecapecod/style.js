903a9b1abcef25a07d3c4ecb0b19ec3bedf79069
Markus <mnordvik@gmail.com>
2014-08-06 18:38:10 -0700

    Update style.js

--- homesourcecapecod/style.js
+++ homesourcecapecod/style.js
@@ -1,1 +1,1 @@
-$(' .footer-holder ').prepend('<li><img src="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/remax_spectrum_logo.jpg"/></li>');
+$(' .footer-links ').prepend('<li><img src="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/remax_spectrum_logo.jpg" width="50%" height="50%"/></li>');
