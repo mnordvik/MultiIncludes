df59c683d4bec788d8fc92d95a6b7f6ed68b3e2e
Markus <mnordvik@gmail.com>
2014-08-07 15:48:12 -0700

    Update style.js

--- leewardislandsvillas/style.js
+++ leewardislandsvillas/style.js
@@ -1,3 +1,9 @@
+debugger;
+
+console.log($(' td:contains("Tax") '));
+
+console.log($(' td:contains("Tax") ').next('td').innerHTML);
+
 $(' td:contains("Tax") ').next('td').html(
   $(' td:contains("Tax") ').next('td').innerHTML.replace('.000','')
 );
