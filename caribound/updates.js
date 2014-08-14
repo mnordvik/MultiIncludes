a67daa4d902d50f924ccb1fe1fc6f0c65e6cbded
Markus <mnordvik@gmail.com>
2014-08-14 11:59:47 -0700

    Update updates.js

--- caribound/updates.js
+++ caribound/updates.js
@@ -1,2 +1,2 @@
 debugger;
-$(' #footer_link_section_three ').html(' <a href="http://www.villabound.com"><img src="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/caribound/villabound.png" style="width:50%;height:50%;" /></a> ');
+$(' #footer_link_section_three ').html(' <a href="http://www.villabound.com"><img src="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/caribound/villabound.png" style="width:100%;height:relative;" /></a> ');
