ba341a47c333fb3cc033ab4bcf82a29c9601e83e
Markus <mnordvik@gmail.com>
2014-11-03 18:01:32 -0800

    Update style.js

--- homesourcecapecod/style.js
+++ homesourcecapecod/style.js
@@ -1,1 +1,1 @@
-$(' .footer-links ').prepend('<li><img src="https://s3.amazonaws.com/jomumist-myvr-prod-photos/inserts/remax_spectrum_logo.jpg" width="50%" height="50%"/></li>');
+$(' .page-banner .tagline ').remove();
